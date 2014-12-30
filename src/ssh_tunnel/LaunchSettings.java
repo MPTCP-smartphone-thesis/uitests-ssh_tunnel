bd2d508f9caa110243f3e80e1f8202ef51a8a958
Matthieu Baerts <matttbe@gmail.com>
2014-12-30 02:26:53 +0100

    Added (not)autoconnect actions to add/remove autoconnect

--- src/ssh_tunnel/LaunchSettings.java
+++ src/ssh_tunnel/LaunchSettings.java
@@ -2,14 +2,22 @@ package ssh_tunnel;
 
 import utils.Utils;
 
+import com.android.uiautomator.core.UiCollection;
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
+import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class LaunchSettings extends UiAutomatorTestCase {
 
 	private static final String ID_TUNNEL_SWITCH = "android:id/checkbox";
 	private static final String ID_CONNECTING = "android:id/message";
+	private static final String ID_LISTVIEW = "android:id/list";
+	private static final String ID_LIST_ELEM_TITLE = "android:id/title";
+	private static final String CLASS_LINEAR_LAYOUT = android.widget.LinearLayout.class
+			.getName();
+	private static final String TEXT_AUTO_CONNECT = "Auto Connect";
 
 	/**
 	 * Start proxy, restart it if already enabled
@@ -17,7 +25,7 @@ public class LaunchSettings extends UiAutomatorTestCase {
 	 * @return true if ok
 	 * @throws UiObjectNotFoundException
 	 */
-	protected boolean start_proxy(UiObject button)
+	protected boolean startProxy(UiObject button)
 			throws UiObjectNotFoundException {
 		// already started?
 		if (button.isChecked()) {
@@ -40,10 +48,45 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		return false;
 	}
 
-	protected void stop_proxy(UiObject button) throws UiObjectNotFoundException {
+	protected void stopProxy(UiObject button) throws UiObjectNotFoundException {
 		if (button.isChecked()) {
 			button.click(); // stop
 			sleep(1000);
+		}
+	}
+
+	private void autoConnect(boolean enable) throws UiObjectNotFoundException {
+		// Scroll the view until finding our file
+		boolean found = false;
+		UiScrollable list = Utils.getScrollableWithId(ID_LISTVIEW);
+		list.setAsVerticalList();
+
+		while (!found) {
+			UiCollection listView = new UiCollection(
+					new UiSelector().resourceId(ID_LISTVIEW));
+			int count = listView.getChildCount(new UiSelector()
+					.className(CLASS_LINEAR_LAYOUT));
+			for (int i = 0; i < count; i++) {
+				UiObject linearLayout = listView.getChild(new UiSelector()
+						.className(CLASS_LINEAR_LAYOUT).instance(i));
+				UiObject title = linearLayout.getChild(new UiSelector()
+						.resourceId(ID_LIST_ELEM_TITLE));
+				if (title.exists() && title.getText().equals(TEXT_AUTO_CONNECT)) {
+					UiObject checkBox = linearLayout.getChild(new UiSelector()
+							.resourceId(ID_TUNNEL_SWITCH));
+					if ((enable && !checkBox.isChecked())
+							|| (!enable && checkBox.isChecked()))
+						assertTrue("Unable to select element",
+								Utils.click(checkBox));
+					found = true;
+					break;
+				}
+			}
+
+			if (!found) {
+				assertTrue("Didn't find the requested object...",
+						Utils.scrollForward(list));
+			}
 		}
 	}
 
@@ -55,10 +98,17 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		// Get button
 		UiObject button = Utils.getObjectWithId(ID_TUNNEL_SWITCH);
 
-		String action = getParams().getString("action"); // default: start
-		if (action == null || !action.equals("stop"))
-			assertTrue("Not able to (re)start the proxy", start_proxy(button));
+		String action = getParams().getString("action");
+		if (action == null) // default: start
+			action = "start";
+
+		if (action.equalsIgnoreCase("stop"))
+			stopProxy(button);
+		else if (action.equalsIgnoreCase("autoconnect"))
+			autoConnect(true);
+		else if (action.equalsIgnoreCase("notautoconnect"))
+			autoConnect(false);
 		else
-			stop_proxy(button);
+			assertTrue("Not able to (re)start the proxy", startProxy(button));
 	}
 }
